6f52e6584994be12a306383bec3d337642045a08
anoninkriz <15214494+antoninkriz@users.noreply.github.com>
2020-07-14 22:29:11 +0200

    Update array_manipulation.cpp

--- array_manipulation.cpp
+++ array_manipulation.cpp
@@ -2,68 +2,29 @@
 
 using namespace std;
 
-class Query {
-public:
-    long from;
-    long to;
-    long amount;
-
-    friend istream &operator>>(istream &is, Query &l) {
-        return is >> l.from >> l.to >> l.amount;
-    }
-
-    friend bool operator<(const Query &a, const Query &b) {
-        return a.from != b.from
-               ? a.from < b.from
-               : a.to != b.to
-                 ? a.to < b.to
-                 : false;
-    }
-};
-
-
-long manipulate(vector<Query> &queries, unordered_map<long, size_t> &levels) {
-    auto max = 0l;
-
-    auto volumes = vector<long>(levels.size());
-
-    for (const auto &q : queries) {
-        auto idxStart = levels[q.from];
-        auto idxEnd = levels[q.to];
-
-        for (auto i = idxStart; i <= idxEnd; i++) {
-            volumes[i] += q.amount;
-
-            if (volumes[i] > max)
-                max = volumes[i];
-        }
-    }
-
-    return max;
-}
-
 int main() {
     ofstream fout(getenv("OUTPUT_PATH"));
 
     int n, m;
     cin >> n >> m;
 
-    vector<Query> queries(m);
-    set<long> levelsSet;
-    for (auto &q : queries) {
-        cin >> q;
-        levelsSet.insert(q.from);
-        levelsSet.insert(q.to);
+    auto counter = map<int, long>();
+
+    for (size_t i = 0; i < m; i++) {
+        int a, b, k;
+        cin >> a >> b >> k;
+
+        counter[a] += k;
+        counter[b + 1] -= k;
     }
 
-    size_t i = 0;
-    unordered_map<long, size_t> levels;
-    for (const auto &l : levelsSet)
-        levels[l] = i++;
+    auto maxSum = 0l, tempSum = 0l;
+    for (const auto &[k, v] : counter) {
+        tempSum += v;
+        maxSum = max(tempSum, maxSum);
+    }
 
-    long result = manipulate(queries, levels);
-
-    fout << result << "\n";
+    fout << maxSum << "\n";
     fout.close();
 
     return 0;
