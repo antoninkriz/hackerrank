a0dcc39f36d89232b49c05281115980d8e8e8a18
anoninkriz <15214494+antoninkriz@users.noreply.github.com>
2020-10-07 18:37:52 +0200

    Added uhunt/11172

---
+++ uhunt/11172_Relational-Operator.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * 11172 - Relational Operator
+ */
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == b)
+            cout << '=' << endl;
+        else if (a < b)
+            cout << '<' << endl;
+        else
+            cout << '>' << endl;
+    }
+
+    return 0;
+}
