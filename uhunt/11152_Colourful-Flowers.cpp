e9a907a34330034ac006d9d145c29f935db008a1
anoninkriz <15214494+antoninkriz@users.noreply.github.com>
2020-10-07 20:04:58 +0200

    Added uhunt/11152

---
+++ uhunt/11152_Colourful-Flowers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * 11152 - Colourful Flowers
+ */
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int x, y, z;
+    while (cin >> x >> y >> z) {
+      double angleX = acos((pow(y, 2) + pow(z, 2) - pow(x, 2)) / (2 * y * z));
+      double rB = x / (2 * sin(angleX));    
+
+      double s = (x + y + z) / 2;
+      double rS = sqrt(s * (s - x) * (s - y) * (s - z)) / s;
+
+      double sB = M_PI * (rB * rB);
+      double sT = sqrt(s * (s - x) * (s - y) * (s - z));
+      double sS = M_PI * (rS * rS);
+
+      cout << fixed << setprecision(4) << sB - sT << " " << sT - sS << " " << sS << "\n";
+    }
+    
+    return 0;
+}
